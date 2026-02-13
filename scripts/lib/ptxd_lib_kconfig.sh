baxeno/ptxdist

commit 3bcb860ca584804859343bb6e961627209f44732

ptxd_kconfig_validate_config: make sure the generated config and diff match
The config and diff are synced so they should match, but if only one of
them is committed to git, then they might be out of sync.

So add another md5 to the diff for the generated config and check that
as well.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_kconfig.sh
@@ -139,7 +139,8 @@ ptxd_kconfig_create_config_merge() {
 	elif [ -n "${conflict}" -a -z "${a}" -a "${b}" = "======" ]; then
 	    continue
 	elif [[ "${a}" =~ ^[a-f0-9]{32}$ && -z "${b}" ]]; then
-	    if [ -z "${conflict}" ]; then
+	    # the first md5 is for the base config
+	    if [ -z "${conflict}" -a -z "${saved_md5}" ]; then
 		saved_md5="${a}"
 	    fi
 	    continue
@@ -318,6 +319,16 @@ ptxd_kconfig_validate_config() {
 		"It does not match '$(ptxd_print_path "${next}")'." \
 		"Run 'oldconfig${p:+ }${p}' to update all layers."
 	fi
+	tmp=( $(md5sum "${last}") )
+	file_md5="${tmp[0]}"
+	saved_md5="$(echo $(sed -n '2p' "${last}.diff"))"
+	# only check if it is an md5 to allow configs from older ptxdist versions
+	if [[ "${saved_md5}" =~ ^[a-f0-9]{32}$ ]] && [ "${file_md5}" != "${saved_md5}" ]; then
+	    local p="${part%ptx}"
+	    ptxd_bailout "'$(ptxd_print_path "${last}.diff")' is inconsistent." \
+		"It does not match '$(ptxd_print_path "${last}")'." \
+		"Run 'oldconfig${p:+ }${p}' to update."
+	fi
     done
     if [ -e "${last}.diff" -a -z "${ignore_last_diff}" ]; then
 	ptxd_bailout  "'$(ptxd_print_path "${last}.diff")' exists without a base layer!"
@@ -461,6 +472,8 @@ ptxd_kconfig_update_config() {
 	local tmp="$(mktemp "${PTXDIST_TEMPDIR}/config.XXXXXX")"
 	set -- $(md5sum "${base_config}")
 	echo "${1}" > "${tmp}" &&
+	set -- $(md5sum "${target_config}") &&
+	echo "${1}" >> "${tmp}" &&
 	sort "${base_config}" > "${tmp}.base" &&
 	sort "${target_config}" > "${tmp}.new" &&
 	# take any new lines and sort by option name

commit c5c5b6581849ac972cca15224c5e2d3493008898

ptxd_kconfig_update_config: fix empty diff handling
The check was broken in 3bcb860ca584804859343bb6e961627209f44732
("ptxd_kconfig_validate_config: make sure the generated config and diff
match") when a second hash was added to the diff header. An 'empty' diff
has two lines now.

We already calculate the hash for both files, so use that to detect if
the diff will be empty and skip generating the diff entirely in this
case.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

scripts/lib/ptxd_lib_kconfig.sh
@@ -470,33 +470,36 @@ ptxd_kconfig_update_config() {
 	ptxd_kconfig_save_config "${target_config}" "${config}" &&
 	echo
     else
+	local base_md5 target_md5
 	local tmp="$(mktemp "${PTXDIST_TEMPDIR}/config.XXXXXX")"
 	set -- $(md5sum "${base_config}")
-	echo "${1}" > "${tmp}" &&
+	base_md5="${1}" &&
 	set -- $(md5sum "${target_config}") &&
-	echo "${1}" >> "${tmp}" &&
-	sort "${base_config}" > "${tmp}.base" &&
-	sort "${target_config}" > "${tmp}.new" &&
-	# take any new lines and sort by option name
-	diff "${tmp}.base" "${tmp}.new" | \
-	    sed -n 's/^> \(\(# \)\?[A-Z]*_\)/\1/p' | \
-	    sed 's/^# \(.*\) is not set$/\1=n/' >> "${tmp}.tmp" &&
-	# handle all removed symbols
-	diff "${tmp}.base" "${tmp}.new" | \
-	    sed -n 's/^< \(# \)\?\([A-Z]*_[^= ]*\)[= ].*$/\2/p' | \
-	    while read sym; do
-		if ! grep -q "^${sym}=" "${tmp}.tmp"; then
-		    echo "${sym}=u" >> "${tmp}.tmp"
-		fi
-	    done
-	sort "${tmp}.tmp" | \
-	    sed -e 's/^\([^=]*\)=n$/# \1 is not set/' \
-		-e 's/^\([^=]*\)=u$/# \1 is undefined/' \
-		>> "${tmp}" &&
-	if [ $(wc -l < "${tmp}") -eq 1 ] && ! cmp -s "${base_config}" "${target_config}"; then
-	    echo "# ptxdist: comment and sorting changes only" >> "${tmp}"
-	fi
-	if [ $(wc -l < "${tmp}") -gt 1 ]; then
+	target_md5="${1}" &&
+	if [ "${base_md5}" != "${target_md5}" ]; then
+	    echo "${base_md5}" > "${tmp}" &&
+	    echo "${target_md5}" >> "${tmp}" &&
+	    sort "${base_config}" > "${tmp}.base" &&
+	    sort "${target_config}" > "${tmp}.new" &&
+	    # take any new lines and sort by option name
+	    diff "${tmp}.base" "${tmp}.new" | \
+		sed -n 's/^> \(\(# \)\?[A-Z]*_\)/\1/p' | \
+		sed 's/^# \(.*\) is not set$/\1=n/' >> "${tmp}.tmp" &&
+	    # handle all removed symbols
+	    diff "${tmp}.base" "${tmp}.new" | \
+		sed -n 's/^< \(# \)\?\([A-Z]*_[^= ]*\)[= ].*$/\2/p' | \
+		while read sym; do
+		    if ! grep -q "^${sym}=" "${tmp}.tmp"; then
+			echo "${sym}=u" >> "${tmp}.tmp"
+		    fi
+		done
+	    sort "${tmp}.tmp" | \
+		sed -e 's/^\([^=]*\)=n$/# \1 is not set/' \
+		    -e 's/^\([^=]*\)=u$/# \1 is undefined/' \
+		    >> "${tmp}" &&
+	    if [ "$(wc -l < "${tmp}")" -eq 2 ]; then
+		echo "# ptxdist: comment and sorting changes only" >> "${tmp}"
+	    fi
 	    ptxd_kconfig_save_config "${target_config}" "${config}" &&
 	    ptxd_kconfig_save_config "${tmp}" "${config}.diff" &&
 	    echo